c165d9577f6053632baf01bff263f05639d48a85
Hafsa-Ahmadi <hafsaahamadi123@gmail.com>
2026-03-04 15:53:08 +0530

    splitMethods.java added

--- splitMethods.java
+++ splitMethods.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class  readTextFromFile {
+public class splitMethods {
 
     public static String readFile(String filepath) {
         try {
@@ -10,17 +10,30 @@ public class  readTextFromFile {
             return content;  
         } catch (IOException e) {
             System.out.println("Error reading file.");
-             return "";
+            return "";
         }
     }
-    //---------------------------------------------------------
 
+    //----------------------------------------------------
+    public static int countWordsSplit(String text) {
+        String[] wordsArray = text.split(" ");
+        int total_words = wordsArray.length;
+        return total_words;
+    }
+    //----------------------------------------------------
+    
     public static void main(String[] args) {
         System.out.println("Welcome to the Word Counter!\n");
 
         String filepath = "input.txt";
         String text = readFile(filepath);
 
-        System.out.println(text);
+        if (text.equals("")) {
+            return;
+        }
+
+        int totalWords =  countWordsSplit(text);
+
+        System.out.println(totalWords);
     }
 }
